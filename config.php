617218b6a6df70ef55e9150df1abacd5fef084d9
Ademiniyi Oreoluwa <106692132+dbigdon@users.noreply.github.com>
2023-03-04 21:53:31 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'kreatifmxnds_smmpanel');
+define('DB_PASS', 'Moroluwa');
+define('DB_NAME', 'kreatifmxnds_smmpanel');
+define('TIMEZONE', 'Africa/Lagos');
+define('ENCRYPTION_KEY', 'bcea142c5fb71a8223ac456955d9b86c');
